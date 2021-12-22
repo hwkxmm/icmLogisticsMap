d4ec7badce6fe9e35a95611c2409e99ff0a48b0c
班未东 <354110809@qq.com>
2021-12-22 10:59:03 +0800

    feat: 增加node启动功能

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+var app = express();
+app.use(express.static(path.resolve(__dirname, './dist')))
+
+
+//监听3002端口
+app.listen(3000, '0.0.0.0', () => {
+    console.log('server is running 3000');
+});
