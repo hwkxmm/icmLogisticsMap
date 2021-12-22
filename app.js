dd3609d9e1c7203742cc8f1488cd1bb422244828
hwkxmm <279693484@qq.com>
2021-12-22 11:13:22 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ app.use(express.static(path.resolve(__dirname, './dist')))
 
 //监听3002端口
 app.listen(3000, '0.0.0.0', () => {
-    console.log('server is running 3000');
+    console.log('server is running 5555');
 });
